a071369b0284f2cd5174c8498a30e09a7432b294
Alberto Daniel Valdez Urquhart <38679181+AlbertV99@users.noreply.github.com>
2020-02-13 08:55:42 -0300

    Actualizacion Archivos core
    Actualizacion de imagenes
    Actualizacion de la carpeta Parametros, inclusion de nuevas funciones al archivo conexion, 
    Creacion de archivos necesarios para la carga de un nuevo DCO

--- Parametros/obtenerDatosQ.php
+++ Parametros/obtenerDatosQ.php
@@ -12,9 +12,37 @@ $resultado=array(array($campos));
 
 $datos=$consultas->consultarDatosQ($campos,$tabla,$orden,$campoC,$valor);
 while ($fila=$datos->fetch_row()) {
+    for ($i=0; $i <count($fila) ; $i++) {
+        $fila[$i]=utf8_encode($fila[$i]);
+    }
     array_push($resultado,$fila);
 }
 array_shift($resultado);
+//echo array_values($resultado);
+//print_r($resultado);
 echo json_encode(array_values($resultado),JSON_PRETTY_PRINT);
+switch (json_last_error()) {
+    case JSON_ERROR_NONE:
+        //echo ' - No errors';
+    break;
+    case JSON_ERROR_DEPTH:
+        echo ' - Maximum stack depth exceeded';
+    break;
+    case JSON_ERROR_STATE_MISMATCH:
+        echo ' - Underflow or the modes mismatch';
+    break;
+    case JSON_ERROR_CTRL_CHAR:
+        echo ' - Unexpected control character found';
+    break;
+    case JSON_ERROR_SYNTAX:
+        echo ' - Syntax error, malformed JSON';
+    break;
+    case JSON_ERROR_UTF8:
+        echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+    break;
+    default:
+        echo ' - Unknown error';
+    break;
+}
 
 ?>
